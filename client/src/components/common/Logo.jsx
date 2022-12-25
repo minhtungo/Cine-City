13ec89e338153c1ee5dfebd59fe33b28c7236c16
minhtungo <loducfc@gmail.com>
2022-12-25 16:41:56 -0500

    Create Global Loading screen

---
+++ client/src/components/common/Logo.jsx
@@ -0,0 +1,12 @@
+import { Typography, useTheme } from '@mui/material';
+
+const Logo = () => {
+  const theme = useTheme();
+
+  return (
+    <Typography fontWeight='700' fontSize='1.7rem'>
+      Cine <span style={{ color: theme.palette.primary.main }}>City</span>
+    </Typography>
+  );
+};
+export default Logo;
